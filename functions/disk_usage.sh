217e0432328ec0026f34f51fd93548ba2a733781
marshki <marshall@morethantomorrow.org>
2018-09-28 12:22:08 -0400

    add disk_usage function

---
+++ functions/disk_usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+disk_usage(){
+  # disk stats: size used avail. capacity in GBs
+  
+  disk_usg=$(df -Ha | awk 'FNR == 2 {print $2,$3,$4,$5}')
+  printf "%s\\n" "$disk_usg (size used avail capacity)" 
+} 
+
+disk_usage
