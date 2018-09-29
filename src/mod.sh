9408cc4ef8b37ad936f4224f05eb5dd2121f1be7
marshki <marshall@morethantomorrow.org>
2018-09-29 08:39:02 -0400

    stable; testing

--- src/mod.sh
+++ src/mod.sh
@@ -2,7 +2,7 @@
 # mjk 2018.09.28 
 
 # header array 
-HEADR=(HOSTNAME UPTIME "LAST LOGIN" "LOAD AVGS." "Running Processes" "Memory Usage" "Disk Usage" "Time")
+HEADR=(HOSTNAME UPTIME "LAST LOGIN" "LOAD AVGS." "RUNNING PROCESSES" "MEMORY USAGE" "DISK USAGE" "TODAY")
 
 # seq increments a period (".") 1-n times, where n is user defined   
 dots(){ 
@@ -15,7 +15,7 @@ dots(){
 host=$(hostname -f)
   
 # uptime: days hours:minutes 
-up_time=$(uptime | sed 's/,//g' | awk '{ print $3,$4,$5}')
+timeup=$(uptime | sed 's/,//g' | awk '{ print $3,$4,$5}')
 
 # last login 
 last_log=$(last | awk 'NR==2'|tr -s ' ')
@@ -23,9 +23,6 @@ last_log=$(last | awk 'NR==2'|tr -s ' ')
 # load avgs. over the past 1,5,15 min. intvls. 
 load_avg=$(uptime | awk -F'[a-z]:' '{print $2}') 
  
-# today: year month hour minute 24-hour timezone(abbr.)
-tdy=$(date +"%Y %B %e, %A, %T %Z")
-
 # running processes
 procs=$(ps ax | wc -l | tr -d " ")
  
@@ -36,15 +33,17 @@ mem=$(top -l 1 -s 0 | awk '/PhysMem/ {print $2,$6}')
 # disk stats: size used avail. capacity in GBs
 disk_usg=$(df -Ha | awk 'FNR == 2 {print $2,$3,$4,$5}') 
 
+# today: year month hour minute 24-hour timezone(abbr.)
+tdy=$(date +"%Y %B %e, %A, %T %Z")
 
 printf "%s" "
   )      |${HEADR[0]}`dots 9`: ${host} (FQDN)        
- (__     |${HEADR[1]}...........: ${timeup}
- _  )_   |${HEADR[2]}.......: ${last_login} 
-(_)_(_)  |${HEADR[3]}....: ${load_avg} (1, 5, 15 min)  
- (o o)   |${HEADR[4]}: ${processes}  
-==\o/==  |${HEADR[5]}.....: ${mem_info} (used unused) 
-         |${HEADR[6]}.......: ${disk_usg} (size used avail capacity) 
-         |${HEADR[7]}:`dots 12`: ${timeup}  
+ (__     |${HEADR[1]}`dots 11`: ${timeup}
+ _  )_   |${HEADR[2]}`dots 7`: ${last_log} 
+(_)_(_)  |${HEADR[3]}`dots 7`: ${load_avg} (1, 5, 15 min)  
+ (o o)   |${HEADR[4]}: ${procs} (total) 
+==\o/==  |${HEADR[5]}`dots 5`: ${mem} (used unused) 
+         |${HEADR[6]}`dots 7`: ${disk_usg} (size used avail capacity) 
+         |${HEADR[7]}:`dots 11`: ${tdy}  
 " 
 
