3776b6019a7ac5771f459d08abf9234a0e729576
marshki <marshall@morethantomorrow.org>
2018-09-29 09:34:49 -0400

    add Linux eth0

--- functions/eth0_ip_addr.sh
+++ functions/eth0_ip_addr.sh
@@ -8,4 +8,15 @@ eth0_ip(){
   printf "%s\\n" "$eth0"
 } 
 
+
+eth0_ip 
+
+eth0_ip(){
+  # get IP address for eth0 (primary)
+  # Linux 
+ 
+  eth0=$(ip route get 1 | awk '{print $NF;exit}')
+  printf "%s\\n" "$eth0"
+} 
+
 eth0_ip
