d84416fd88b0adfa5fdc089ce4c25b1c70113b49
marshki <marshall@morethantomorrow.org>
2018-09-30 10:39:15 -0400

    re-factor

--- functions/dots.sh
+++ functions/dots.sh
@@ -1,4 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash 
+
+HEADR=(HOSTNAME TODAY) 
+
+host=$(hostname -f)
+tdy=$(date +"%Y %B %e, %A, %T %Z")
+
+hr() {
+  # shellcheck disable=SC2183
+  # print horizontal line of characters 
+
+  printf '%*s\n' "${1:-$COLUMNS}" | tr ' ' "${2:-#}"
+}
+
+printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" "${host}"
+printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${tdy}"
+
+
+
+
+
+
+
+
+
 
 dots(){ 
   # seq increments a period (".") 1-n times, where n is user defined   
