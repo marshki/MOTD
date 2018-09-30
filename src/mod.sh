4a721573f0e4dcadcce81f6cfdbbf29e187c1c12
marshki <marshall@morethantomorrow.org>
2018-09-30 11:01:19 -0400

    further layer of abstraction

--- src/mod.sh
+++ src/mod.sh
@@ -4,11 +4,11 @@
 # header array 
 HEADR=(HOSTNAME UPTIME "LAST LOGIN" "LOAD AVGS." "RUNNING PROCESSES" "MEMORY USAGE" "DISK USAGE" "TODAY")
 
-# seq increments a period (".") 1-n times, where n is user defined   
-dots(){ 
-  printf "%s" "$2"; 
-  printf "%.0s." $( seq $(($1-${#2})) );
-  echo; 
+hr() {
+  # shellcheck disable=SC2183
+  # print horizontal line of characters 
+
+  printf '%*s\n' "${1:-$COLUMNS}" | tr ' ' "${2:-#}"
 }
 
 # fully-qualified domain name 
@@ -36,15 +36,23 @@ disk_usg=$(df -Ha | awk 'FNR == 2 {print $2,$3,$4,$5}')
 # today: year month hour minute 24-hour timezone(abbr.)
 tdy=$(date +"%Y %B %e, %A, %T %Z")
 
-printf "%s" "
-  )      |${HEADR[0]}`dots 9`: ${host} (FQDN)        
- (__     |${HEADR[1]}`dots 11`: ${timeup}
- _  )_   |${HEADR[2]}`dots 7`: ${last_log} 
-(_)_(_)  |${HEADR[3]}`dots 7`: ${load_avg} (1, 5, 15 min)  
- (o o)   |${HEADR[4]}: ${procs} (total) 
-==\o/==  |${HEADR[5]}`dots 5`: ${mem} (used unused) 
-         |${HEADR[6]}`dots 7`: ${disk_usg} (size used avail capacity) 
-         |${HEADR[7]}:`dots 11`: ${tdy}  
+printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" "${host}"
+printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${timeup}"
+printf -- '%.30s: %s\n' "| ${HEADR[2]}$(hr 30 .)" "${last_log}"
+printf -- '%.30s: %s\n' "| ${HEADR[3]}$(hr 30 .)" "${load_avg}"
+printf -- '%.30s: %s\n' "| ${HEADR[4]}$(hr 30 .)" "${procs}"
+printf -- '%.30s: %s\n' "| ${HEADR[5]}$(hr 30 .)" "${mem}"
+printf -- '%.30s: %s\n' "| ${HEADR[6]}$(hr 30 .)" "${disk_usg}"
+printf -- '%.30s: %s\n' "| ${HEADR[7]}$(hr 30 .)" "${tdy}"
 
-" 
+#printf "%s" "
+#  )      |${HEADR[0]}`dots 9`: ${host} (FQDN)        
+# (__     |${HEADR[1]}`dots 11`: ${timeup}
+# _  )_   |${HEADR[2]}`dots 7`: ${last_log} 
+#(_)_(_)  |${HEADR[3]}`dots 7`: ${load_avg} (1, 5, 15 min)  
+# (o o)   |${HEADR[4]}: ${procs} (total) 
+#==\o/==  |${HEADR[5]}`dots 5`: ${mem} (used unused) 
+#         |${HEADR[6]}`dots 7`: ${disk_usg} (size used avail capacity) 
+#         |${HEADR[7]}:`dots 11`: ${tdy}  
+#" 
 
