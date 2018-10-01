84a4f73ecb379c34135234d6549ca2304ffbcf89
marshki <marshall@morethantomorrow.org>
2018-10-01 12:12:46 -0400

    update

--- functions/os_check.sh
+++ functions/os_check.sh
@@ -24,24 +24,47 @@ printf '
 }
 
 # fully-qualified domain name 
-host=$(hostname -f)
+# host=$(hostname -f)
 
-# get MACADDR for primary 
-mac_osx=$(ifconfig en1 | awk '/ether/{print $2}')
-mac=$(cat /sys/class/net/eth0/address)
+host_name(){
+  # fully-qualified domain name 
+  
+  host=$(hostname -f)
+  printf "%s\\n" "$host (FQDN)"  
+}
+
+addr_o(){
+  # get MACADDR for primary
+  # OS X
+
+  mac=$(ifconfig en1 | awk '/ether/{print $2}')
+  printf "%s\\n" "$mac"
+}
+
+addr_l(){
+  # get MACADDR for primary
+  # Linux
+
+  mac=$(cat /sys/class/net/eth0/address)
+  printf "%s\\n" "$mac"
+}
+
+
+#mac_osx=$(ifconfig en1 | awk '/ether/{print $2}')
+#mac=$(cat /sys/class/net/eth0/address)
   
 case $(uname -s) in
 Darwin)
   maus
   printf "%s\\n" "Darwin"
-  printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" "${host} (FQDN)"
-  printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${mac_osx}"
+  printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" host_name"#"${host} (FQDN)"
+  printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" addr_o"#"${mac_osx}"
   ;;
 Linux)
   printf "%s\\n" "Linux" 
   maus
-  printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" "${host} (FQDN)"
-  printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${mac}"
+  printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" host_name"#"${host} (FQDN)"
+  printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" addr_l"#"${mac}"
   ;;
 *)
   printf "%s\\n" "He can't handle your speed, $(uname -m)"
