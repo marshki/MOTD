67e48b9a09a07a87ea98f2f57af153c3570aa2c4
marshki <marshall@morethantomorrow.org>
2018-10-01 13:15:30 -0400

    remove excess code

--- src/mod.sh
+++ src/mod.sh
@@ -94,17 +94,3 @@ Linux)
   printf "%s\\n" "He can't handle your speed, $(uname -m)"
   ;;
 esac
-
-
-
-#maus
-#printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" "${host} (FQDN)"
-#printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${timeup}"
-#printf -- '%.30s: %s\n' "| ${HEADR[2]}$(hr 30 .)" "${last_log}"
-#printf -- '%.30s: %s\n' "| ${HEADR[3]}$(hr 30 .)" "${load_avg} (1 min 5 mins 15 mins)"
-#printf -- '%.30s: %s\n' "| ${HEADR[4]}$(hr 30 .)" "${procs} (total)"
-#printf -- '%.30s: %s\n' "| ${HEADR[5]}$(hr 30 .)" "${mem} (used unused)"
-#printf -- '%.30s: %s\n' "| ${HEADR[6]}$(hr 30 .)" "${disk_usg} (size used avail capacity)"
-#printf -- '%.30s: %s\n' "| ${HEADR[7]}$(hr 30 .)" "${tdy}"
-
-
