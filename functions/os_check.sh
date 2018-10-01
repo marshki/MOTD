b891fcc59dd57a002f4b9ab7588a30d904472fe0
marshki <marshall@morethantomorrow.org>
2018-10-01 10:58:28 -0400

    testing

--- functions/os_check.sh
+++ functions/os_check.sh
@@ -1,7 +1,19 @@
-#!/bin/bash 
+#!/usr/bin/env bash 
 
-os_check(){
-  # check if OS is UNIX of LINUX
+case $(uname -s) in
+Darwin)
+  printf "%s\\n" "Darwin"
+  ;;
+Linux)
+  printf "%s\\n" "Linux" 
+  ;;
+*)
+  printf "%s\\n" "He can't handle your speed, $(uname -m)"
+  ;;
+esac
+
+#os_check(){
+  # check if OS is LINUX or UNIX 
   # uname -s 
   # Need to check against this 
-} 
+#} 
