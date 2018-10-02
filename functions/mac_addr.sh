65fb83f81e1fca5881ac5a7e37e54a4a05903fc6
marshki <marshall@morethantomorrow.org>
2018-10-02 10:39:14 -0400

    refactor to include OS check

--- functions/mac_addr.sh
+++ functions/mac_addr.sh
@@ -1,21 +1,24 @@
-#!/bin/bash 
+#!/usr/bin/env bash 
+# Check OS; return primary MAC 
+  
+case $(uname -s) in
+Darwin)
 
-mac_addr(){
-  # get MACADDR for primary  
-  # OS X 
- 
+  # get MACADDR for primary in OS X  
   mac=$(ifconfig en1 | awk '/ether/{print $2}')
   printf "%s\\n" "$mac"
-} 
-mac_addr 
+  ;;
 
-mac_addr(){
-  # get MACADDR for primary  
-  # Linux  
- 
+Linux)
+  
+  # get MACADDR for primary in Linux  
   mac=$(cat /sys/class/net/eth0/address)
+  
   printf "%s\\n" "$mac"
-} 
+  ;;
 
-mac_addr
+*)
+  printf "%s\\n" "He can't handle your speed, $(uname -s)"
+  ;;
+esac
 
