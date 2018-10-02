6f0ec285b44aa86c7f570da9b7929df5096e5d28
marshki <marshall@morethantomorrow.org>
2018-10-02 09:54:48 -0400

    clean out excess code

--- functions/os_check.sh
+++ functions/os_check.sh
@@ -1,60 +1,14 @@
 #!/usr/bin/env bash 
-
-#Header array 
-HEADR=(HOSTNAME "MAC ADDRESS")  
-
-hr(){
-  # shellcheck disable=SC2183
-  # print horizontal line of characters 
-
-  printf '%*s\n' "${1:-$COLUMNS}" | tr ' ' "${2:-#}"
-}
-
-maus() {
-  # ASCII mouse 
-
-printf '
-  )            
- (__
- _  )_
-(_)_(_)
- (o o)
-==\o/==
-'
-}
-
-host_name(){
-  # fully-qualified domain name 
-  
-  host=$(hostname -f)
-  printf "%s\\n" "$host (FQDN)"  
-}
-
-# fully-qualified domain name 
-#host=$(hostname -f)
-
   
 case $(uname -s) in
 Darwin)
-  # MAC ADDRESS
-  mac_osx=$(ifconfig en1 | awk '/ether/{print $2}')
-
-  maus
   printf "%s\\n" "Darwin"
-  printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" `host_name` #"${host} (FQDN)" 
-  printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${mac_osx}"
   ;;
 Linux)
-  # MAC ADDRESS
-  mac=$(cat /sys/class/net/eth0/address)
-  
-  maus
   printf "%s\\n" "Linux" 
-  printf -- '%.30s: %s\n' "| ${HEADR[0]}$(hr 30 .)" `host_name` #"${host} (FQDN)"
-  printf -- '%.30s: %s\n' "| ${HEADR[1]}$(hr 30 .)" "${mac}" 
   ;;
 *)
-  printf "%s\\n" "He can't handle your speed, $(uname -m)"
+  printf "%s\\n" "He can't handle your speed, $(uname -s)"
   ;;
 esac
 
