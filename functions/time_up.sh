4071cb80672e40bc5f768616238ac200f507b199
marshki <marshall@morethantomorrow.org>
2018-10-09 15:37:03 -0400

    update comments

--- functions/time_up.sh
+++ functions/time_up.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash  
+# uptime: days hours:minutes 
 
 time_up(){ 
-  # uptime: days hours:minutes 
-  
+  # uptime
+  # strip commas, then print columns 3,4,5 
+    
   timeup=$(uptime | sed 's/,//g' | awk '{ print $3,$4,$5}')
   printf "%s\\n" "$timeup" 
 }
