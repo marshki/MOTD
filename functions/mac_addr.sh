e359483b7d15f4a42144f8b68f3c9aeaa105b3c5
marshki <marshall@morethantomorrow.org>
2018-10-09 15:10:05 -0400

    comments

--- functions/mac_addr.sh
+++ functions/mac_addr.sh
@@ -5,13 +5,18 @@ case $(uname -s) in
 Darwin)
 
   # get MACADDR for primary in OS X  
+  # get mac addr for en1, 
+  # then regex for ether, print 2nd column
+
   mac=$(ifconfig en1 | awk '/ether/{print $2}')
+
   printf "%s\\n" "$mac"
   ;;
 
 Linux)
   
   # get MACADDR for primary in Linux  
+
   mac=$(cat /sys/class/net/eth0/address)
   
   printf "%s\\n" "$mac"
