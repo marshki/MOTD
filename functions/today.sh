738d7b2eec858737bdbf9242dd76bd8e85274ea9
marshki <marshall@morethantomorrow.org>
2018-10-09 15:37:54 -0400

    comments

--- functions/today.sh
+++ functions/today.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash 
+# today: year month hour minute 24-hour timezone(abbr.)
 
 today(){
-  # today: year month hour minute 24-hour timezone(abbr.)
-  
+    
   tdy=$(date +"%Y %B %e, %A, %T %Z")
   printf "%s \\n" "$tdy" 
 } 
