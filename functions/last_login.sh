07d870753653caefb5fef3577a839832865ed360
marshki <marshall@morethantomorrow.org>
2018-10-09 11:21:42 -0400

    comments

--- functions/last_login.sh
+++ functions/last_login.sh
@@ -2,8 +2,11 @@
 
 last_login(){ 
   # last login 
-  
+  # print second line of last command 
+  # strip out sequence of repeated characters (' ', i.e. whitespace)
+ 
   last_log=$(last | awk 'NR==2'|tr -s ' ')
+
   printf "%s\\n" "$last_log"
 } 
 
