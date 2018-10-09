473596632c61c14b043e73795ea54295649dd5a8
marshki <marshall@morethantomorrow.org>
2018-10-09 10:29:41 -0400

    documentation

--- functions/disk_usage.sh
+++ functions/disk_usage.sh
@@ -2,6 +2,8 @@
 
 disk_usage(){
   # disk stats: size used avail. capacity in GBs
+  # disk space in human-readable format (all mounts points)
+  # awk second row, then take columns 2-5 inclusive
   
   disk_usg=$(df -Ha | awk 'FNR == 2 {print $2,$3,$4,$5}')
   printf "%s\\n" "$disk_usg (size used avail capacity)" 
