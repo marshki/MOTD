9ec04ba6a9b257adf9c985e8505572e6492d717d
marshki <marshall@morethantomorrow.org>
2018-11-15 11:35:32 -0500

    rework mac_addr function for Linux

--- functions/mac_addr.sh
+++ functions/mac_addr.sh
@@ -15,11 +15,10 @@ Darwin)
 
 Linux)
   
-  # get MACADDR for primary in Linux  
-  # probe all in ifconfig, 
-  # grep for 1st match, then perl regular expressions to print content after HWaddr string 
+  # get MACADDR for primary in Linux 
+  # ip addr for eth0, then awk to match ether and extract
 
-  mac=$(ifconfig -a | grep -m1 --perl-regexp --only-matching 'HWaddr \K.*$)  
+  mac=$(ip addr show eth0 |awk '/ether/{print $2}')  
   
   printf "%s\\n" "$mac"
   ;;
