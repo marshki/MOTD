04fa065aecf3c7f601b28056a94c0c767f3c2f14
marshki <marshall@morethantomorrow.org>
2019-01-19 08:59:27 -0500

    create a function for protability

--- functions/bulk_rename.sh
+++ functions/bulk_rename.sh
@@ -1,2 +1,20 @@
-# placing here so as not to forget
-for f in SF2*; do mv "$f" "${f/SF2/SF_spDist1}";done
+#!/bin/bash 
+# Bulk rename of files 
+# mjk235 [at] nyu [dot] edu
+
+
+# string to find in file name 
+find_string=""
+
+# string to replace in file name 
+replace_string=""
+
+bulk_rename() { 
+  for file in $find_string; do 
+    mv "$file" "$(file/$find_string/$replace_string}"
+  done
+} 
+
+#for f in SF_spDist1*; do mv "$f" "${f/SF_spDist1/SF2}";done
+
+#for f in SF2*; do mv "$f" "${f/SF2/SF_spDist1}";done
