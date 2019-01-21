656be0cca2d5e21629159ae68bd5b114e5ac26ff
marshki <marshall@morethantomorrow.org>
2019-01-21 07:46:10 -0500

    working script add

--- functions/bulk_rename.sh
+++ functions/bulk_rename.sh
@@ -1,25 +1,23 @@
 #!/bin/bash 
 # mjk235 [at] nyu [dot] edu
+# Bulk rename of files that start with: `x` 
 
-# Bulk rename of files beginning with x 
+# Read user input & assign to: `find_string`. 
 
-# string to find in file name 
-find_string="SF2"
-
-# string to replace in file name 
-replace_string="SF_spDist1"
-
-
-string_to_find() {
-  read -p "Enter the string you want to find: " 
-
+input_to_find() {
+  read -p "Enter the string to find: " find_string 
 } 
 
-#string_to_replace() {} 
+# Read user input & assign to: `replace_string`. 
 
-confirm_change() { 
+input_to_replace() {
+  read -p "Enter the string to replace: " replace_string 
+} 
 
-  read -p "Confirm rename of files from '$find_string' to '$replace_string'(y/n)? " answer 
+# Read user input; exit if not Y or y.  
+
+confirm_changes() { 
+  read -p "Confirm change from:'$find_string' to:'$replace_string'(y/n)? " answer 
    
   if [ "$answer" != "${answer#[Yy]}" ] ;then
       printf "%s\n" "Continuing..."
@@ -30,21 +28,28 @@ confirm_change() {
 } 
 
 bulk_rename() {
+  printf "%s\n" "Renaming files..."
  
-  for file in $find_string; do 
-    mv "$file" "${file/$find_string/$replace_string}"
+  for file in $find_string*; do 
+    mv -v "$file" "${file/$find_string/$replace_string}"
   done
+
+  printf "%s\n" "Done."
 } 
 
 # Main 
 
-main() { 
-  confirm_change 
-  #bulk_rename
+printf "%s\n" "Bulky: A Bash script to rename files en masse."
+
+main() {
+  input_to_find
+  input_to_replace 
+  confirm_changes
+  bulk_rename
 } 
 
 main "$@" 
 
-#### One-liners from a terminal: 
+#### One-liners, e.g.:  #### 
 #for f in SF_spDist1*; do mv "$f" "${f/SF_spDist1/SF2}";done
 #for f in SF2*; do mv "$f" "${f/SF2/SF_spDist1}";done
