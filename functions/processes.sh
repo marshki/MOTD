ea7e0dc98e614c71387b49aeb4b3158cf5c5b1c5
marshki <marshall@morethantomorrow.org>
2021-09-09 14:18:44 -0400

    rm extra /

--- functions/processes.sh
+++ functions/processes.sh
@@ -6,7 +6,7 @@ processes(){
   # wc lines, then tr to delete whitespace  
 
   procs=$(ps ax | wc -l | tr -d " ")
-  printf "%s\\n" "$procs (total)" 
+  printf "%s\n" "$procs (total)" 
 } 
 
 processes
