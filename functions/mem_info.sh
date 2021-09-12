4ca8c86a7069c9b704317000e3a46eabad942b86
marshki <marshall@morethantomorrow.org>
2021-09-12 14:07:10 -0400

    add sample outputs, top & awk

--- functions/mem_info.sh
+++ functions/mem_info.sh
@@ -22,15 +22,17 @@ Linux)
 
   memory=$(free -g |awk 'FNR==2 {print $3, $4}')
 
-  #-->work on this, need to print the proper row and columns <<- 
-  # top -n 1 -d 0|awk '/Mem/'
-
-  # Sample outputs
+  # Sample outputs:
   # MiB Mem :   7661.1 total,   2233.0 free,   2893.5 used,   2534.6 buff/cache
   # KiB Mem :  1698324 total,   711904 free,   419296 used,   567124 buff/cache
   # Mem:  132149488k total,  3042460k used, 129107028k free,   957356k buffers
   # KiB Mem : 13201608+total, 12153704+free,  2608656 used,  7870376 buff/cache
+  # KiB Mem : 11550142+total, 10699744+free,  2259544 used,  6244432 buff/cache
 
+  # Extract using this: 
+  # top -n 1 -d 0 | awk '/Mem/'
+  # top -n 1 -d 0 | awk 'NR==4'
+  
   printf "%s\n" "$memory (used unused)"
   ;;
 
