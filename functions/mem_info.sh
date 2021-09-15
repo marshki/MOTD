2056713507b236174bb31c1528c909e9c8203f36
marshki <marshall@morethantomorrow.org>
2021-09-15 12:25:53 -0400

    rm comments, fix spacing

--- functions/mem_info.sh
+++ functions/mem_info.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env bash 
-# Check OS; return free and used memory based on result. 
+#!/usr/bin/env bash
+# Check OS; return free and used memory based on result.
   
 case $(uname -s) in
 
 Darwin)
   
-  # memory stats: used unused
+  # memory stats: total unused
   # top sample once, delay 0 secs
   # awk regex, return columns 2,6
   # --> KiB is returned in truncated format, which can throw off results <--
 
   memory=$(top -l 1 -s 0 | awk '/PhysMem/ {print $2,$6}')
   
-  printf "%s\n" "$memory (total unused)" 
+  printf "%s\n" "$memory (total unused)"
   ;;
 
 Linux)
   
-  # memory stats: used unused 
+  # memory stats: total unused
   # free, then awk second row, print columns 3,4
 
   memory=$(free -g |awk 'FNR==2 {print $2, $4}')
