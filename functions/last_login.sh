764fb299f40c40ad039b3c0e95ab11689b4ecfe1
marshki <marshall@morethantomorrow.org>
2021-09-15 12:56:27 -0400

    fix spacing

--- functions/last_login.sh
+++ functions/last_login.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
-last_login(){ 
-  # last login 
-  # print second line of last command 
+last_login(){
+  # last login
+  # print second line of last command
   # strip out sequence of repeated characters (' ', i.e. whitespace)
  
   last_log=$(last | awk 'NR==2'|tr -s ' ')
 
   printf "%s\\n" "$last_log"
-} 
+}
 
 last_login
