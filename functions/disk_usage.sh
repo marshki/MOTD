ab5df4013e575043ff79510a8f75d06888ee9ed2
marshki <marshall@morethantomorrow.org>
2021-09-15 13:08:14 -0400

    address spacing

--- functions/disk_usage.sh
+++ functions/disk_usage.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 # disk stats: size used avail. capacity in GBs
 
 disk_usage(){
@@ -6,7 +6,7 @@ disk_usage(){
   
   disk_usg=$(df -Ha | awk 'FNR == 2 {print $2,$3,$4,$5}')
 
-  printf "%s\\n" "$disk_usg (size used avail capacity)" 
-} 
+  printf "%s\\n" "$disk_usg (size used avail capacity)"
+}
 
 disk_usage
