6766a7bdd125228eaba9e188d9581c09213b8ae2
marshki <marshall@morethantomorrow.org>
2021-09-15 13:03:51 -0400

    update comments; fix spacing; substitue / for // printf

--- functions/eth0_ip_addr.sh
+++ functions/eth0_ip_addr.sh
@@ -1,28 +1,28 @@
-#!/usr/bin/env bash 
-# Check OS; return primary IP 
+#!/usr/bin/env bash
+# Check OS; return primary IP.
   
 case $(uname -s) in
 Darwin)
   
-  # get IP address for eth0 (primary) in OS X 
-  eth0=$(ipconfig getifaddr en0) 
+  # get IP address for eth0 (primary) in macOS. 
+  eth0=$(ipconfig getifaddr en0)
 
-  printf "%s\\n" "$eth0"
+  printf "%s\n" "$eth0"
   ;;
 
 Linux)
 
-  # get IP address for eth0 (primary) in Linux 
-  # ip add for eth0, then print last column
+  # get IP address for eth0 (primary) in Linux.
+  # ip add for eth0, then print last column.
 
   eth0=$(ip route get 1 | awk '{print $NF;exit}')
 
-  printf "%s\\n" "$eth0"
+  printf "%s\n" "$eth0"
 
   ;;
 
 *)
-  printf "%s\\n" "He can't handle your speed, $(uname -s)"
+  printf "%s\n" "He can't handle your speed, $(uname -s)"
   ;;
 esac
 
