f957781efc172b303393f5e46c4fcddbaafeb878
marshki <marshall@morethantomorrow.org>
2023-10-13 10:40:26 -0400

    add spacing

--- functions/today.sh
+++ functions/today.sh
@@ -2,9 +2,10 @@
 # Today: date command formatted as: 
 # year month hour minute 24-hour timezone(abbr.)
 
-today(){
+today() {
     
   today=$(date +"%Y %B %e, %A, %T %Z")
+
   printf "%s \n" "$today" 
 } 
 
