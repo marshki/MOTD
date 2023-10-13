652bc5ff05296f71081f4305b638500a9370870c
marshki <marshall@morethantomorrow.org>
2023-10-13 10:26:12 -0400

    spacing

--- functions/processes.sh
+++ functions/processes.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash  
+#!/usr/bin/env bash
 # running processes
 
-processes(){ 
-  # ps all user's processes 
-  # wc lines, then tr to delete whitespace  
+processes() {
+  # ps all user's processes
+  # wc lines, then tr to delete whitespace
 
   procs=$(ps ax | wc -l | tr -d " ")
-  printf "%s\n" "$procs (total)" 
-} 
+
+  printf "%s\n" "$procs (total)"
+}
 
 processes
