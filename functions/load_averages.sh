417132682706806f3ee2e9fd51b39a0d48dc9293
marshki <marshall@morethantomorrow.org>
2023-10-13 10:06:30 -0400

    update

--- functions/load_averages.sh
+++ functions/load_averages.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash 
 # load avgs. over the past 1,5,15 min. intvls.
 
-load_averages(){ 
+load_averages() {
   # uptime, then extract string to the left of (:)
  
-  load_avg=$(uptime |awk '{ sub(/^.*: /, ""); print; }')  
+  load_avg=$(uptime |awk '{ sub(/^.*: /, ""); print; }')
 
-  printf "%s\n" "$load_avg (1, 5, 15 min)" 
+  printf "%s\n" "$load_avg (1, 5, 15 min)"
 }
 
 load_averages
-
