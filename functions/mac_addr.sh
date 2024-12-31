b19a9b30805d32011ea41be886245fc313af5e6f
marshki <marshall@morethantomorrow.org>
2024-12-31 13:10:57 -0500

    rm excess spacing; add comments

--- functions/mac_addr.sh
+++ functions/mac_addr.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
-# Check OS; return primary MAC
+# Check OS; return primary MAC address.
   
 case $(uname -s) in
 Darwin)
-
-  # get MACADDR for primary in OS X  
-  # get mac addr for en0, 
+  # get MACADDR for primary in macOS
+  # get mac addr for en0,
   # then regex for ether, print 2nd column
-
   mac=$(ifconfig en0 | awk '/ether/{print $2}')
-
   printf "%s\\n" "$mac"
   ;;
-
 Linux)
-  
-  # get MACADDR for primary in Linux 
+  # get MACADDR for primary in Linux
   # ip addr for eth0, then awk to match ether and extract
-
   mac=$(ip addr show eth0 |awk '/ether/{print $2}')
-  
   printf "%s\\n" "$mac"
   ;;
-
 *)
   printf "%s\\n" "He can't handle your speed, $(uname -s)"
   ;;
 esac
-
