2beff28c15ca907e037c10d9ec42e5bf1f3c755b
marshki <marshall@morethantomorrow.org>
2024-12-31 13:23:28 -0500

    refactor

--- functions/eth0_ip_addr.sh
+++ functions/eth0_ip_addr.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
-# Check OS; return primary IP.
+# Check OS and return primary IP.
   
 case $(uname -s) in
 Darwin)
-  
-  # get IP address for eth0 (primary) in macOS.
+  # get IP address for eth0 (primary) in macOS
   eth0=$(ipconfig getifaddr en0)
-
   printf "%s\n" "$eth0"
   ;;
-
 Linux)
-
-  # get IP address for eth0 (primary) in Linux.
-  # ip add for eth0, then print last column.
-
-  eth0=$(ip route get 1 | awk '{print $NF;exit}')
-  
-  # Simpler, but need to test:
-  # hostname -I |awk '{printf $1}'
-
+  # get IP address for eth0 (primary) in GNU/Linux
+  hostname --all-ip-addresses |awk '{printf $1}'
   printf "%s\n" "$eth0"
-
   ;;
-
 *)
   printf "%s\n" "He can't handle your speed, $(uname -s)"
   ;;
