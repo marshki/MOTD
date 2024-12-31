ca1693a7dc2c977982a89666ea4c9bf7976e7313
marshki <marshall@morethantomorrow.org>
2024-12-31 13:31:45 -0500

    update

--- functions/dots.sh
+++ functions/dots.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # print horizontal line of x characters y times,
-# in this case a 'dot'
+# in this case a 'dot'.
 
 dot() {
   # shellcheck disable=SC2183
   # print (#) to column width
   # print (.) to column position 30
-
   printf '%*s\n' "${1:-$COLUMNS}" | tr ' ' "${2:-#}"
 }
 
