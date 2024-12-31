dd26761d89733dd0030ff3814e81f569000132ca
marshki <marshall@morethantomorrow.org>
2024-12-31 12:39:47 -0500

    remove spacing

--- functions/today.sh
+++ functions/today.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash 
-# Today: date command formatted as: 
+#!/usr/bin/env bash
+# Today: date command formatted as:
 # year month hour minute 24-hour timezone(abbr.)
 
 today() {
-    
   today=$(date +"%Y %B %e, %A, %T %Z")
-
-  printf "%s \n" "$today" 
-} 
+  printf "%s \n" "$today"
+}
 
 today
