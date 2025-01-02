5c2726b73031cf937bd4632ea4fdb8b619092b78
marshki <marshall@morethantomorrow.org>
2025-01-02 09:41:09 -0500

    rm excess \ in printf

--- functions/last_login.sh
+++ functions/last_login.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# Last login.
+# Last logged in user.
 
 last_login() {
   # print second line of last command
   # strip out sequence of repeated characters (' ', i.e. whitespace)
   last_log=$(last | awk 'NR==2'|tr -s ' ')
-  printf "%s\\n" "$last_log"
+  printf "%s\n" "$last_log"
 }
 
 last_login
