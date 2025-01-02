01c5aa08a7780da47f3e1f588ae654f8e1288218
marshki <marshall@morethantomorrow.org>
2025-01-01 21:52:41 -0500

    update

--- functions/last_login.sh
+++ functions/last_login.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
+# Last login.
 
 last_login() {
-  # last login
   # print second line of last command
   # strip out sequence of repeated characters (' ', i.e. whitespace)
- 
   last_log=$(last | awk 'NR==2'|tr -s ' ')
-
   printf "%s\\n" "$last_log"
 }
 
--- functions/load_averages.sh
+++ functions/load_averages.sh
@@ -1,11 +1,9 @@
-#!/usr/bin/env bash 
-# load avgs. over the past 1,5,15 min. intvls.
+#!/usr/bin/env bash
+# CPU load avgs. over the past 1,5,15 min. intervals.
 
 load_averages() {
   # uptime, then extract string to the left of (:)
- 
   load_avg=$(uptime |awk '{ sub(/^.*: /, ""); print; }')
-
   printf "%s\n" "$load_avg (1, 5, 15 min)"
 }
 
