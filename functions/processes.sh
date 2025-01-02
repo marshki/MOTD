c321958f9825bfa578228f5abeefb31742dce539
marshki <marshall@morethantomorrow.org>
2025-01-02 10:17:50 -0500

    update

--- functions/processes.sh
+++ functions/processes.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# running processes
+# Total # of running processes.
 
 processes() {
   # ps all user's processes
   # wc lines, then tr to delete whitespace
-
   procs=$(ps ax | wc -l | tr -d " ")
-
   printf "%s\n" "$procs (total)"
 }
 
