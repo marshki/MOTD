311ae20dabc749edbdb3b056d11825e8854fa6e0
marshki <marshall@morethantomorrow.org>
2025-01-02 09:44:42 -0500

    rm excess \ in printf

--- functions/mac_addr.sh
+++ functions/mac_addr.sh
@@ -7,15 +7,15 @@ Darwin)
   # get mac addr for en0,
   # then regex for ether, print 2nd column
   mac=$(ifconfig en0 | awk '/ether/{print $2}')
-  printf "%s\\n" "$mac"
+  printf "%s\n" "$mac"
   ;;
 Linux)
   # get MACADDR for primary in Linux
   # ip addr for eth0, then awk to match ether and extract
   mac=$(ip addr show eth0 |awk '/ether/{print $2}')
-  printf "%s\\n" "$mac"
+  printf "%s\n" "$mac"
   ;;
 *)
-  printf "%s\\n" "He can't handle your speed, $(uname -s)"
+  printf "%s\n" "He can't handle your speed, $(uname -s)"
   ;;
 esac
