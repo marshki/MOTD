f8e4806401c723fd70567a04ed6cdbaf08245d84
marshki <marshall@morethantomorrow.org>
2025-01-08 11:37:37 -0500

    refactor

--- functions/time_up.sh
+++ functions/time_up.sh
@@ -3,9 +3,14 @@
 
 time_up() {
   # uptime
-  # strip commas, then print columns 3,4,5
-  timeup=$(uptime | sed 's/,//g' | awk '{ print $3,$4,$5}')
-  printf "%s\n" "$timeup"
+  # sed to stip commas
+  # awk to print columns, depending on if uptime is days or hours
+  local uptime_info=$(uptime | sed 's/,//g')
+  if [[ "$uptime_info" == *"day"* ]]; then
+    printf "%s\n" "$uptime_info" | awk '{ print $3, $4, $5 }'
+  else
+    printf "%s\n" "$uptime_info" | awk '{ print $3 }'
+  fi
 }
 
 time_up
