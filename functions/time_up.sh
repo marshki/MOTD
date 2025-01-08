00a792c85122974c85b4a0aa95ba38eed077e196
marshki <marshall@morethantomorrow.org>
2025-01-08 11:42:10 -0500

    add sample output to comments

--- functions/time_up.sh
+++ functions/time_up.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # uptime: days hours:minutes
+# e.g.: "1 day, HH:MM" or: "HH:MM"
 
 time_up() {
   # uptime
   # sed to stip commas
-  # awk to print columns, depending on if uptime is days or hours
+  # awk to print columns (depending on if uptime is days or hours)
   local uptime_info=$(uptime | sed 's/,//g')
   if [[ "$uptime_info" == *"day"* ]]; then
     printf "%s\n" "$uptime_info" | awk '{ print $3, $4, $5 }'
