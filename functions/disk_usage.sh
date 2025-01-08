dd968a01e4811818ddfdf89c2fb94fd3a4f8c119
marshki <marshall@morethantomorrow.org>
2025-01-08 11:57:28 -0500

    rm excess spacing

--- functions/disk_usage.sh
+++ functions/disk_usage.sh
@@ -3,9 +3,7 @@
 
 disk_usage() {
   # awk second row, then take columns 2-5 inclusive
-  
   disk_usg=$(df -Ha | awk 'FNR == 2 {print $2,$3,$4,$5}')
-
   printf "%s\\n" "$disk_usg (size used avail capacity)"
 }
 
