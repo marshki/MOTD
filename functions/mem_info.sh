9480908cb42b35c4e42f2ef6c79476bec2851d83
marshki <marshall@morethantomorrow.org>
2025-01-10 12:21:41 -0500

    refactor memory function for GNU/Linux

--- functions/mem_info.sh
+++ functions/mem_info.sh
@@ -13,7 +13,7 @@ Darwin)
 Linux)
   # memory stats: total unused
   # free, then awk second row, print columns 3,4
-  memory=$(free -g |awk 'FNR==2 {print $2, $4}')
+  memory=$(free --human |awk 'FNR==2 {print $2, $6}')
   printf "%s\n" "$memory (total unused)"
   ;;
 *)
