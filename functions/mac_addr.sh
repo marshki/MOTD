b396e1ac537747105a8ea980bafe53636a55adf6
marshki <marshall@morethantomorrow.org>
2025-01-13 14:37:43 -0500

    refactor

--- functions/mac_addr.sh
+++ functions/mac_addr.sh
@@ -1,36 +1,14 @@
 #!/usr/bin/env bash
-# Check OS; return primary MAC address.
+# Check OS and return primary MAC address.
   
-case $(uname -s) in
-Darwin)
-  # get MACADDR for primary in macOS
-  # get mac addr for en0,
-  # then regex for ether, print 2nd column
-  mac=$(ifconfig en0 | awk '/ether/{print $2}')
-  printf "%s\n" "$mac"
-  ;;
-Linux)
-  # get MACADDR for primary in GNU/Linux
-  # ip addr for eth0, then awk to match ether and extract
-  mac=$(ip addr show eth0 |awk '/ether/{print $2}')
-  printf "%s\n" "$mac"
-  ;;
-*)
-  printf "%s\n" "He can't handle your speed, $(uname -s)"
-  ;;
-esac
-
-
-# Alternative approach
-# GNU/Linux
-ip link | awk '/ether/ {print $2; exit}'
-# macOS
-ifconfig | awk '/ether/ {print $2; exit}'
-# and then: 
 macaddr=$(
   if [[ "$(uname -s)" == "Darwin" ]]; then
-    ifconfig | awk '/ether/ {print $2; exit}'
+    route -n get default 2>/dev/null | awk -F': ' '/interface:/ {print $2}' | \
+      xargs ifconfig | awk '/ether/ {print $2}'
   else
-    ip link | awk '/ether/ {print $2; exit}'
-  fi
+    ip route | awk '/default/ {print $5}' | \
+      xargs ip addr show | awk '/ether/ {print $2}'
+    fi
 )
+
+printf "%s\n" "$macaddr"
