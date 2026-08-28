37323b90b57f3431dfc5888a9bdd5de3115ee4c1
marshki <marshall@morethantomorrow.org>
2026-08-28 12:58:21 -0400

    add printf statement

--- functions/disk_usage.sh
+++ functions/disk_usage.sh
@@ -3,8 +3,9 @@
 
 disk_usage() {
   # awk second row, then take columns 2-5 inclusive
-  disk_usg=$(df -H | awk '$NF == "/" { print $2 " total, " $3 " used, " $4 " available (" $5 ")" }') 
-  printf "%s\\n" "$disk_usg"
+  disk_usg=$(df -h --output=size,used,avail,pcent,target |
+    awk '$5 == "/" {print $1 " total, " $2 " used, " $3 " available (" $4 ")"}')
+  printf "%s\n" "$disk_usg"
 }
 
 disk_usage
