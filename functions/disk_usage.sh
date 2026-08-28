3ec5ed6e4b84cd56cef8613dd75b4274b0377727
marshki <marshall@morethantomorrow.org>
2026-08-28 12:24:25 -0400

    update comments & clean up print statements

--- functions/disk_usage.sh
+++ functions/disk_usage.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# disk stats: size used avail. capacity in GBs
+# disk stats: total used avail. capacity in GBs
 
 disk_usage() {
   # awk second row, then take columns 2-5 inclusive
-  disk_usg=$(df -H | awk '$NF == "/" { print $2, $3, $4, $5 }')
-  printf "%s\\n" "$disk_usg (size used avail %capacity)"
+  disk_usg=$(df -H | awk '$NF == "/" { print $2 " total, " $3 " used, " $4 " available (" $5 ")" }') 
+  printf "%s\\n" "$disk_usg"
 }
 
 disk_usage
