fbc0c63335a7ac14db050384d0d5194ef402751b
marshki <marshall@morethantomorrow.org>
2026-08-28 13:13:49 -0400

    update comment and clarify what % means

--- functions/disk_usage.sh
+++ functions/disk_usage.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# disk stats: total used avail. capacity in GBs
+# disk stats: total used available %capacity in GBs
 
 disk_usage() {
   # awk second row, then take columns 2-5 inclusive
   disk_usg=$(df -h --output=size,used,avail,pcent,target |
-    awk '$5 == "/" {print $1 " total, " $2 " used, " $3 " available (" $4 ")"}')
+    awk '$5 == "/" {print $1 " total, " $2 " used, " $3 " available (" $4 " used)"}')
   printf "%s\n" "$disk_usg"
 }
 
