a07452aa0ef8ce7ebe64e2a099b1eef5dce1c1b0
marshki <marshall@morethantomorrow.org>
2026-08-28 12:10:39 -0400

    update comment

--- functions/load_averages.sh
+++ functions/load_averages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# CPU load avgs. over the past 1,5,15 min. intervals.
+# CPU load averages over the past 1, 5, 15 min. intervals.
 
 load_averages() {
-  # uptime, then extract string to the left of (:)
+  # uptime, then strip everything to the left of (:)
   load_average=$(uptime |awk '{ sub(/^.*: /, ""); print; }')
   printf "%s\n" "$load_average (1, 5, 15 min)"
 }
